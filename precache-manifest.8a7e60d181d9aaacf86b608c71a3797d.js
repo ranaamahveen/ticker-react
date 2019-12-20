d5aa944ed0c06b8eb3119841b30f902c3238db12
Ranaa <rm0043@mix.wvu.edu>
2019-12-20 14:22:02 -0500

    Updates

---
+++ precache-manifest.8a7e60d181d9aaacf86b608c71a3797d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae3333197fa6e632fa17d9f58f5e80d",
+    "url": "/ticker-react/index.html"
+  },
+  {
+    "revision": "dcac48971ad505f68d45",
+    "url": "/ticker-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "2f56cfebd346b2e574a6",
+    "url": "/ticker-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dcac48971ad505f68d45",
+    "url": "/ticker-react/static/js/2.b11151e6.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/ticker-react/static/js/2.b11151e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f56cfebd346b2e574a6",
+    "url": "/ticker-react/static/js/main.5283c105.chunk.js"
+  },
+  {
+    "revision": "d8154b6a62bef2ba2276",
+    "url": "/ticker-react/static/js/runtime-main.8aa183ee.js"
+  }
+]);
